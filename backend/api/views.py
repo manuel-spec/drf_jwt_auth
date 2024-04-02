f5fea28237116ebb4aebfdb205524bccbcdd64c1
manuel-spec <amaniase084@gmail.com>
2024-04-02 07:06:20 -0700

    login backend with jwt done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 # Create your views here.
+class LoginView(APIView):
+    def post(self, request):
+      username = request.data.get('username')
+      password = request.data.get('password')
+      user = authenticate(username=username, password=password)
+      refresh = RefreshToken.for_user(user)
+      return JsonResponse({
+         "refresh":str(refresh),
+         "access":str(refresh.access_token)
+      })
+      
